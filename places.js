35f6259595cc7aa6b46210c86cb95c255c63d5d4
kudal2 <dante._15@hotmail.com>
2022-06-26 18:21:34 -0400

    samandos

--- places.js
+++ places.js
@@ -8,17 +8,11 @@ function staticLoadPlaces() {
        {
            name: 'asset',
            location: {
-               lat: 10.243782370641224,
-               lng: -68.01064753535684,
+               lat: 10.236125,
+               lng: -68.006586,
            }
        },
-       {
-        name: 'assetdos',
-        location: {
-            lat: 10.246169,
-            lng: -68.009979,
-        }
-    },
+
        
    ];
 }
@@ -31,21 +25,13 @@ function renderPlaces(places) {
        let longitude = place.location.lng;
 
        let model = document.createElement('a-entity');
-       model.setAttribute('gps-entity-place', `latitude: 10.243782370641224; longitude: -68.01064753535684;`);
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
        model.setAttribute('gltf-model', 'assets/asset.gltf');
        model.setAttribute('rotation', '0 270 0');
        model.setAttribute("position", "4 0.1 0");
        model.setAttribute('animation-mixer', '');
        model.setAttribute('scale', '.5 .5 .5');
-
-
-       let modeldos = document.createElement('a-entity');
-       model.setAttribute('gps-entity-place', `latitude:10.246169 ; longitude: -68.009979;`);
-       model.setAttribute('gltf-model', 'assets/cedro.gltf');
-       model.setAttribute('rotation', '0 270 0');
-       model.setAttribute("position", "4 0.1 0");
-       model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '.5 .5 .5');
+       
 
        model.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
