00c9f5d2f331256672296bfcac71c6b07871dadf
kudal2 <dante._15@hotmail.com>
2022-06-26 18:01:20 -0400

    cedro

--- places.js
+++ places.js
@@ -24,8 +24,17 @@ function renderPlaces(places) {
        let longitude = place.location.lng;
 
        let model = document.createElement('a-entity');
-       model.setAttribute('gps-entity-place', `simulateLatitude: ${latitude}; simulateLongitude: ${longitude};`);
+       model.setAttribute('gps-entity-place', `latitude: 10.243782370641224; longitude: -68.01064753535684;`);
        model.setAttribute('gltf-model', 'assets/asset.gltf');
+       model.setAttribute('rotation', '0 270 0');
+       model.setAttribute("position", "4 0.1 0");
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '.5 .5 .5');
+
+
+       let modeldos = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: 10.246169; simulateLongitude: -68.009979;`);
+       model.setAttribute('gltf-model', 'assets/cedro.gltf');
        model.setAttribute('rotation', '0 270 0');
        model.setAttribute("position", "4 0.1 0");
        model.setAttribute('animation-mixer', '');
